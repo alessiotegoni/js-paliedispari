cc034fcb583fccba0092679d751abae5127424fc
tega <alessiotegoni4@gmail.com>
2024-10-18 13:32:33 +0200

    [ADD] palindromic word completed exercise

---
+++ palindromic.js
@@ -0,0 +1,22 @@
+const getUserPrompt = () => prompt("Inserisci una parola");
+
+function checkPalindromicWord() {
+  const userWord = getUserPrompt();
+
+  let reversedString = "";
+  let isPalindromic = false;
+
+  if (!userWord) return isPalindromic;
+
+  for (let i = userWord.length - 1; i >= 0; i--) {
+    reversedString += userWord.charAt(i);
+  }
+
+  if (reversedString === userWord) isPalindromic = true;
+
+  console.log(isPalindromic);
+
+  return isPalindromic;
+}
+
+checkPalindromicWord();
